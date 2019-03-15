48e24408fabd038612d546f1aae6b1628017811c
Swapnil Sawant <swapnil.sawant@arabidopsis.org>
2019-03-14 17:14:41 -0700

    expand ellipsis text to full text in table cell

--- src/components/table/TableCellD3.vue
+++ src/components/table/TableCellD3.vue
@@ -2,7 +2,7 @@
    <svg :width=tdWidth :height=tdHeight>
        <g>
            <text v-if="cellText != '*'"
-                 dy=".35em" x=5 y=20>{{computedText}}</text>
+                 dy=".35em" x=5 y=20 style='word-wrap: break-word'>{{computedText}}</text>
            <circle v-if="cellText == '*'" class="anno_circle"
                  cx="100" cy="18"></circle>
        </g>
@@ -55,15 +55,19 @@
                         // and show ellipsis. When you hover over the ellipsis text it will show
                         // tooltip with full text. (Todo)
                         if(computedLength > this.cellWidth) {
-                            this.computedText = this.cellText.slice(0, 17);
-                            this.computedText += "..."; 
+                            let splitText = this.cellText.slice(0, 17);
+                            this.computedText = splitText + "..."; 
                             this.isEllipsis = true;
-                            let remainingText = this.cellText.slice(15, this.cellText.length);
+                            let remainingText = this.cellText.slice(17, this.cellText.length);
                             this.el.on("mouseover", () => {
-                                this.el.select('text').text(remainingText);
+                                this.tdHeight = '40px';
+                                this.el.select('text').attr('y', 15).text(splitText);
+                                this.el.select('text').append('tspan')
+                                    .attr('x', 5).attr('y', 35).text(remainingText);
                             })
                             .on("mouseout", () => {
-                                this.el.select('text').text(this.computedText);
+                                this.tdHeight = '30px';
+                                this.el.select('text').attr('y', 20).text(this.computedText);
                             });
 
                         }
