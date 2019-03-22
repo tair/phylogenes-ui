5d1b735558aeead6074e113ecd31c133659c8e3c
Qian Li <qianli1987@arabidopsis.org>
2019-03-22 16:10:59 -0700

    PHG-113

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="elevation-0 px-5 bg-pggrey">
             <div class="container-fluid px-3" style="max-width: 1600px">
-                        <p class="lead text-dark py-3 my-0 text-bold" style="text:bold;"><b>PhyloGenes displays pre-computed phylogenetic trees of protein families alongside experimental gene function data and sequence information to facilitate inference of unknown gene function in plants.</b></p>
+                        <p class="lead text-dark py-3 my-0 text-bold" style="text:bold;"><b>PhyloGenes displays pre-computed phylogenetic trees of gene families alongside experimental gene function data to facilitate inference of unknown gene function in plants.</b></p>
             </div>
         </div>
     </div>
