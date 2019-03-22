2f12d4d846da68289eb0a8f613e0d4d02005c05e
Qian Li <qianli1987@arabidopsis.org>
2019-03-22 12:12:41 -0700

    PHG-134 help link to conf page

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -20,40 +20,15 @@
                         </a>
                     </router-link>
 
-<!--                     <li class="nav-item dropdown pr-2">
-                        <a class="nav-link dropdown-toggle" style="font-size: 15px" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-cogs"></i>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <router-link to="/about">
-                                <a class="dropdown-item">
-                                    <span>About</span>
-                                </a>
-                            </router-link>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li> -->
-
                     <router-link to="/about" tag="li">
                         <a class="nav-link pr-3" style="font-size: 15px"  data-toggle="tooltip" title="About Us">
                             About
                         </a>
                     </router-link>
 
-                    <router-link to="/help" tag="li">
-                        <a class="nav-link pr-3" style="font-size: 15px"  data-toggle="tooltip" title="Help">
-                            Help
-                        </a>
-                    </router-link>
-
-<!--                     <router-link to="/tree" tag="li">
-                        <a class="nav-link pr-3" style="font-size: 15px"  data-toggle="tooltip" title="Explore trees">
-                            <i class="fas fa-tree"></i>
-                        </a>
-                    </router-link> -->
+                    <a href="https://conf.arabidopsis.org/display/PHGSUP" class="nav-link pr-3" style="font-size: 15px"  data-toggle="tooltip" title="Help">
+                        Help
+                    </a>
 
                     <router-link to="/contact" tag="li">
                         <a class="nav-link pr-3" style="font-size: 15px"  data-toggle="tooltip" title="Contact Us">
@@ -75,11 +50,6 @@
                             </button>
                         </form>
                     </li>
-<!--                     <router-link to="/login" tag="li">
-                        <a class="nav-link pr-3" style="font-size: 15px"  data-toggle="tooltip" title="Login with ORCID">
-                            <i class="fas fa-sign-in-alt"></i>
-                        </a>
-                    </router-link> -->
 
                 </ul>
 
