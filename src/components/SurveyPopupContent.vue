61d9ece641a8074e814c18e2ca4d1129293c8612
swapp <swapp19902@gmail.com>
2020-09-10 16:36:50 -0700

    add survey link as popup

---
+++ src/components/SurveyPopupContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="surveyText">
+    <pre>
+We need your feedback! In less than 5 minutes, you can plan the future development of PhyloGenes. 
+
+Please take the short survey after your visit to PhyloGenes today and tell us your thoughts. 
+  
+Survey link: Users in China 
+<a
+  href="https://www.wenjuan.com/s/UbiuIfJ/"
+  target="_blank"
+>https://www.wenjuan.com/s/UbiuIfJ/</a>
+
+Survey link: Users elsewhere in the world 
+<a
+  href="https://www.surveymonkey.com/r/9ZT25V3"
+  target="_blank"
+>https://www.surveymonkey.com/r/9ZT25V3</a>
+</pre>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.surveyText {
+  font-size: 20px;
+}
+</style>
--- src/components/modal/CustomModal.vue
+++ src/components/modal/CustomModal.vue
@@ -1,25 +1,19 @@
 <template>
   <transition name="modal">
     <div class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
+      <div class="modal-wrapper" @click="$emit('close')">
+        <div class="modal-container" :style="modal_style">
           <div class="modal-header">
-            <slot name="header">
-              default header
-            </slot>
+            <slot name="header">default header</slot>
           </div>
 
           <div class="modal-body align-middle">
-            <slot name="body">
-              default body
-            </slot>
+            <slot name="body">default body</slot>
           </div>
 
           <div class="modal-footer">
             <slot name="footer">
-              <button class="modal-default-button" @click="$emit('close')">
-                OK
-              </button>
+              <button class="modal-default-button" @click="$emit('close')">OK</button>
             </slot>
           </div>
         </div>
@@ -31,12 +25,20 @@
 <script>
 export default {
   name: 'customModal',
+  props: ['modalWidth'],
+  mounted() {
+    if (this.modalWidth) {
+      this.modal_style = 'width: ' + this.modalWidth + 'px'
+    }
+  },
   components: {},
   data() {
-    return {}
+    return {
+      modal_style: 'width: 500px'
+    }
   },
   computed: {},
-  methods: {},
+  methods: {}
 }
 </script>
 
