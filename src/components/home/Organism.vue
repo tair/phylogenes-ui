7fd39d069a85d4489793228db3f6694a66999dd7
swapp1990 <swapnil.sawant@arabidopsis.org>
2022-11-14 20:07:56 +0530

    update version update type of download menu

--- src/components/home/Organism.vue
+++ src/components/home/Organism.vue
@@ -23,7 +23,7 @@
       <div class="scroll-div bg-white my-2 px-5" v-if="showText">
         <p class="text-lg mr-5 pr-5">
           Protein sequences from these plant species are included in the current
-          PhyloGenes release (version 4.0):
+          PhyloGenes release (version 4.1):
         </p>
         <div class="row">
           <div class="col-sm-12 col-md-6 col-lg-6">
